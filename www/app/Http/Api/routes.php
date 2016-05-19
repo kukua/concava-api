794e5f9a889f019ece2d4eb4546b1c0e30bd98d5
Maurits van Mastrigt <maurits@nerdieworks.nl>
2016-05-19 15:17:47 +0200

    Add cross origin headers.

--- www/app/Http/Api/routes.php
+++ www/app/Http/Api/routes.php
@@ -1,5 +1,10 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+header('Access-Control-Allow-Credentials: true');
+
 Route::get('users/login', ['as' => 'users.login', 'uses' => 'UserController@login']);
 Route::resource('users', 'UserController');
 Route::resource('devices', 'DeviceController');
