d0354e42b8e360b62b4fb69a366e6d5dba52fa18
Maurits van Mastrigt <maurits@nerdieworks.nl>
2016-05-30 14:27:32 +0200

    Remove 'password_confirmation' from registering user (resolve #11).

--- www/app/Models/User.php
+++ www/app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	static $rules = [
 		'name' => 'required|max:255',
 		'email' => 'required|email',
-		'password' => 'required|confirmed|min:8|max:255'
+		'password' => 'required|min:8|max:255'
 	];
 	protected $fillable = ['name', 'email', 'password'];
 	protected $hidden = ['password', 'remember_token'];
